a65a1c4ef52e80b412655e38e8fedcdee0adf9bd
indikaudagedara <indikaudagedara@users.noreply.github.com>
2015-02-05 13:58:55 +1100

    Create project.sh

---
+++ project.sh
@@ -0,0 +1,235 @@
+if [[ $# -lt 1 ]]; then
+        echo "$0 option [arg]"
+        echo "  i(init) [project]"
+        echo "  s(set) project"
+        echo "  d(delete) [project]"
+        echo "  a(add) directory"
+        echo "  l(list project)"
+        echo "  e(edit project)"
+        echo "  la(list all projects)"
+        return
+fi
+
+DIR=~/projects
+
+_exec() {
+        cmd=$1
+        echo "$cmd"
+        eval $cmd
+}
+
+init_proj() {
+        proj=$1
+
+        echo "--> init $proj"
+
+        pf=$DIR/${proj}.project
+
+        # cscope files
+        csf=$DIR/${proj}.files
+
+        # cscope cross reference
+        csrf=$DIR/${proj}.out
+
+        ctf=$DIR/${proj}.tags
+
+        touch $pf
+        touch $csf
+        touch $csrf
+        touch $ctf
+
+        for dir in `cat $pf`; do
+                _exec "find $dir/. -type f | egrep -i \"\.(c|h|cpp)$\" >> $csf"
+        done
+
+        sort -u -o $csf $csf
+
+        lns=`wc -l $csf | cut -f 1 -d ' '`
+
+        if [[ "$lns" != "0" ]]; then
+
+
+                # build cscope cross ref
+                _exec "cscope -b -i $csf -f $csrf"
+                _exec "ctags -L $csf -f $ctf"
+        fi
+}
+
+
+set_proj() {
+
+        proj=$1
+
+        DIR=~/projects
+        pf=$DIR/${proj}.project
+
+        if [[ ! -e $pf ]]; then
+                echo "no $pf"
+                return 0
+        fi
+
+        echo "--> set $proj"
+
+        # cscope files
+        csf=$DIR/${proj}.files
+
+        # cscope cross reference
+        csrf=$DIR/${proj}.out
+
+        ctf=$DIR/${proj}.tags
+
+        lns=`wc -l $csf | cut -f 1 -d ' '`
+        echo "files=$lns"
+        if [[ "$lns" != "0" ]]; then
+                export FILES_DB=$csf
+        else
+                export FILES_DB=""
+        fi
+
+
+        lns=`wc -l $csrf | cut -f 1 -d ' '`
+        echo "csope=$lns"
+        if [[ "$lns" != "0" ]]; then
+                export CSCOPE_DB=$csrf
+        else
+                export CSCOPE_DB=""
+        fi
+
+        lns=`wc -l $ctf | cut -f 1 -d ' '`
+        echo "ctags=$lns"
+        if [[ "$lns" != "0" ]]; then
+                export TAGS_DB=$ctf
+        else
+                export TAGS_DB=""
+        fi
+
+        export PROJECT=$proj
+
+        return 1
+}
+
+del_proj() {
+        proj=$1
+
+        echo "--> del $proj"
+
+        pf=$DIR/${proj}.project
+
+        # cscope files
+        csf=$DIR/${proj}.files
+
+        # cscope cross reference
+        csrf=$DIR/${proj}.out
+
+        ctf=$DIR/${proj}.tags
+
+        rm -f $pf $csf $csrf $ctf
+}
+
+check_proj() {
+
+        if [[ "$PROJECT" == "" ]]; then
+                echo "project not set"
+                return 0
+        fi
+
+        echo "--> $PROJECT"
+        return 1
+}
+
+case $1 in
+        i)
+                proj=""
+                if [[ $# -ne 2 ]]; then
+                        proj=$PROJECT
+                else
+                        proj=$2
+                fi
+
+                if [[ "$proj" == "" ]]; then
+                        echo "set project or pass a new one"
+                        return
+                fi
+                init_proj $proj
+                set_proj $proj
+                return
+        ;;
+
+        s)
+                set_proj $2
+                return
+        ;;
+
+        d)
+                proj=""
+                if [[ $# -ne 2 ]]; then
+                        proj=$PROJECT
+                else
+                        proj=$2
+                fi
+
+                if [[ "$proj" == "" ]]; then
+                        echo "--> set project or pass existing one"
+                        return
+                fi
+
+                del_proj $proj
+                return
+
+        ;;
+
+
+        a)
+
+                if [[ $# -ne 2 ]]; then
+                        echo "--> pass a directory"
+                        return
+                fi
+
+                check_proj; [[ $? != 1 ]] && return
+
+                proj=$PROJECT
+                pf=$DIR/${proj}.project
+                echo "$PWD/$2" >> $pf
+
+                sort -u -o $pf $pf
+                init_proj $proj
+                return
+
+        ;;
+
+        l)
+                check_proj; [[ $? != 1 ]] && return
+
+                proj=$PROJECT
+                pf=$DIR/${proj}.project
+                cat $pf
+                return
+
+        ;;
+
+        e)
+                check_proj; [[ $? != 1 ]] && return
+
+                proj=$PROJECT
+                pf=$DIR/${proj}.project
+                vim $pf
+                return
+        ;;
+
+        la)
+                if [[ "$PROJECT" != "" ]]; then
+                        echo "--> $PROJECT"
+                fi
+
+                for i in `ls $DIR/*.project`; do
+                        basename $i | sed 's/\.project//'
+                done
+                return
+        ;;
+
+        *)
+                echo "--> unexpected option"
+                return
+        ;;
+esac
